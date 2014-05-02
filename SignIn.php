a76d57c7a37d4811a990657a616cf2e450dc2c07
Bondili Rohan Singh <operations.vellore@makeadiff.in>
2014-05-02 07:45:11 +0530

    [Added] Sign In Page
    Sign In page for all users.
    Depending on the user group page will be redirected.

---
+++ SignIn.php
@@ -0,0 +1,30 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="MIT SANA Clean Hive">
+    <meta name="author" content="Bondili Rohan Singh">
+    <link rel="shortcut icon" href="assets/icons/favicon.ico">
+    <title>Clean Hive | Sign In</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Sign In</h2>
+        <input type="email" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" class="form-control" placeholder="Password" required>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
+      </form>
+
+    </div> 
+  </body>
+</html>
