e06767bf35d9571c2e9987ea0256204d3b1b0496
Ipsita Prakash <ipsita16@hotmail.com>
2014-05-02 20:13:03 +0530

    Commit 5

--- Ovuguide/gen/com/example/ovuguide/R.java
+++ Ovuguide/gen/com/example/ovuguide/R.java
@@ -25,24 +25,28 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int header_background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int item_background=0x7f020003;
-        public static final int item_background_focused=0x7f020004;
-        public static final int list_item_background=0x7f020005;
-        public static final int muchi=0x7f020006;
+        public static final int custommucus=0x7f020000;
+        public static final int header_background=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int item_background=0x7f020004;
+        public static final int item_background_focused=0x7f020005;
+        public static final int list_item_background=0x7f020006;
+        public static final int muchi=0x7f020007;
+        public static final int mucus=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
+        public static final int action_settings=0x7f09000a;
         public static final int button1=0x7f090000;
-        public static final int date=0x7f090006;
-        public static final int date_icon=0x7f090007;
-        public static final int gridview=0x7f090005;
-        public static final int header=0x7f090001;
-        public static final int next=0x7f090004;
-        public static final int previous=0x7f090002;
-        public static final int title=0x7f090003;
+        public static final int button2=0x7f090002;
+        public static final int button3=0x7f090001;
+        public static final int date=0x7f090008;
+        public static final int date_icon=0x7f090009;
+        public static final int gridview=0x7f090007;
+        public static final int header=0x7f090003;
+        public static final int next=0x7f090006;
+        public static final int previous=0x7f090004;
+        public static final int title=0x7f090005;
     }
     public static final class layout {
         public static final int activity_display=0x7f030000;
